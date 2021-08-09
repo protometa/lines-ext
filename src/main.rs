cb724b14f3d023bd773326d37ee77fa6a319d683
Luke Nimtz <luke.nimtz@gmail.com>
2021-08-09 09:52:25 -0700

    basic cumulative scanning

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use async_std::io;
 use async_std::io::prelude::*;
 use async_std::io::{stdin, BufReader, Cursor};
-use futures::future::{self, Future};
+use futures::future::{self, Future, Ready};
 use futures::stream::{self, Stream, StreamExt, TryStream, TryStreamExt};
 // use clap::{App, Arg};
 
@@ -11,10 +11,12 @@ async fn main() -> Result<()> {
     println!("Hello World");
     let cursor = Cursor::new(
         b"
-            ---
-            a: 1
-            ---
-            a: 2
+---
+a: 1
+b: 2
+---
+a: 2
+b: 3
         ",
     );
     let lines = cursor.lines();
@@ -22,8 +24,17 @@ async fn main() -> Result<()> {
     let lines: Vec<String> = lines
         .scan(
             String::new(),
-            |state, line| -> futures::future::Ready<Option<Result<String, std::io::Error>>> {
-                future::ready(Some(Ok(state.clone())))
+            |state, line| -> Ready<Option<Result<String, std::io::Error>>> {
+                future::ready(
+                    line.map(|line| {
+                        state.push('\n');
+                        state.push_str(&line);
+                        Some(state.trim().to_owned())
+                    })
+                    .transpose(),
+                )
+                // // take result of option and transpose to option of result
+                // future::ready(Some(Ok(state.clone())))
             },
         )
         .try_collect()
