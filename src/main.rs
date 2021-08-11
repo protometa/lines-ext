e11060949e5dd2923d96afde1c65076a9f317347
Luke Nimtz <luke.nimtz@gmail.com>
2021-08-10 18:27:24 -0700

    separate function with passing tests

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,32 @@
 use anyhow::Result;
 use async_std::io;
 use async_std::io::prelude::*;
-use async_std::io::{stdin, BufReader, Cursor};
+use async_std::io::{stdin, BufRead, BufReader, Cursor, Lines};
 use futures::future::{self, Future, Ready};
 use futures::stream::{self, Stream, StreamExt, TryStream, TryStreamExt};
 // use clap::{App, Arg};
 
-#[async_std::main]
-async fn main() -> Result<()> {
-    println!("Hello World");
-    let cursor = Cursor::new(
-        b"
----
-a: 1
-b: 2
----
-a: 2
-b: 3
-        ",
-    );
-    let lines = cursor.lines();
-
-    let docs: Vec<String> = lines
-        .chain(stream::once(async { Ok("---".to_string()) }))
+pub fn chunk_by_line<R: BufRead>(
+    lines: Lines<R>,
+    delimiter: &str,
+) -> impl Stream<Item = Result<String, std::io::Error>> {
+    let delimiter1 = delimiter.to_owned();
+    let delimiter2 = delimiter.to_owned();
+    lines
+        .chain(stream::once(async move { Ok(delimiter1) }))
         // Stream of Result<String>
         .scan(
             String::new(),
-            |state, line| -> Ready<Option<Result<Option<String>, std::io::Error>>> {
+            move |state, line| -> Ready<Option<Result<Option<String>, std::io::Error>>> {
                 future::ready(
                     line.map(|line| {
-                        Some(if line == "---" {
-                            let chunk = state.trim().to_owned();
+                        Some(if line == delimiter2 {
+                            let chunk = state.to_owned();
                             state.clear();
                             Some(chunk)
                         } else {
-                            state.push('\n');
                             state.push_str(&line);
+                            state.push('\n');
                             None
                         })
                     })
@@ -47,14 +38,33 @@ b: 3
         .try_filter_map(|x| future::ready(Ok(x)))
         // Stream of Result<String>
         .try_filter(|x| future::ready(x.len() > 0))
-        .try_collect()
-        .await?;
+}
 
-    dbg!(docs);
-
-    // while let Some(line) = lines.next().await {
-    //     dbg!(line?);
-    // }
-
+#[async_std::main]
+async fn main() -> Result<()> {
+    println!("Hello World");
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[async_std::test]
+    async fn chunks_by_line() -> Result<()> {
+        let lines = Cursor::new(
+            b"~~~
+multi
+line
+chunk
+~~~
+another
+chunk
+",
+        )
+        .lines();
+        let docs: Vec<String> = chunk_by_line(lines, "~~~").try_collect().await?;
+        assert_eq!(docs, vec!["multi\nline\nchunk\n", "another\nchunk\n"]);
+        Ok(())
+    }
 }
